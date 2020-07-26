f318bc10b343c15a93b32fb502eb28be3ce053d6
Pranjal Sharma <nkak306500@gmail.com>
2020-07-26 21:47:39 +0530

    Update apiConfig.js
    removed apiKey

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -4,7 +4,7 @@ module.exports = {
     "default_success_message":"Successfuly generated the report",
     "ibm_watson_config":{
         version:'2017-10-13',
-        apikey: '_LQ_jy1x50lKwVJl8iOnbajCaaVFVV7_ShMi2GU5ISOW',
+        apikey: '',
         url: 'https://api.eu-gb.personality-insights.watson.cloud.ibm.com/instances/0f5114c4-ea4e-4f1e-ab69-46fc0b1015b1'   
     },
     "validation_message":"Description field is missing or it's empty"
